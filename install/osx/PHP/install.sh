0c2700c5fc8c87aeedd6845915cc5073815a005f
Patrick Selge <patrick.selge@th-wildau.de>
2016-08-20 14:28:51 +0200

    add laravel

--- install/osx/PHP/install.sh
+++ install/osx/PHP/install.sh
@@ -3,5 +3,6 @@ PKG_DIR=$(dirname $0)
 
 brew bundle --file=$PKG_DIR/Brewfile
 
-composer global require laravel/valet
+composer global require "laravel/installer"
+composer global require "laravel/valet"
 $HOME/.composer/vendor/bin/valet install
